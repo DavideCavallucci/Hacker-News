50f18eb1b36f1786f8906fec75a6d81427f8e6a4
DavideCavallucci <davide1706@gmail.com>
2025-01-05 21:40:43 +0100

    Nuovo webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+/*const path = require('path');
 const Dotenv = require('dotenv-webpack'); // Per gestire le variabili d'ambiente
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // Per generare il file HTML con asset inclusi automaticamente
 const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // Per estrarre CSS in file separati
@@ -65,4 +65,50 @@ module.exports = {
   performance: {
     hints: false,
   },
+};*/
+
+
+
+
+
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: './src/js/app.js', // input
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js', // output
+    clean: true, // per pulire la cartella 'dist' a ogni build
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // regola per i file CSS
+        use: ['style-loader', 'css-loader'], // loader per il CSS
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i, // regola per le immagini
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html', // template di base per l'HTML
+      favicon: './src/img/favicon.ico', // gestione della favicon automatica
+    }),
+    new Dotenv(),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'), // cartella per il bundle
+    },
+    compress: true, // compressione gzip
+    port: 9000, // porta del server
+    open: true, // apertura automatica del browser
+  },
+  mode: 'development', // modalità di sviluppo o pruduzione
 };
